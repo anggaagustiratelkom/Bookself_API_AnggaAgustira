cd2fde77f96f9187bfb575d58e8c650963cc2ad9
anggaagustiratelkom <anggaagustira@gmail.com>
2022-11-28 01:13:59 +0700

    env

--- src/handler.js
+++ src/handler.js
@@ -1,9 +1,16 @@
-const { nanoid } = require('nanoid');
-const books = require('./books');
+const { nanoid } = require("nanoid");
+const books = require("./books");
 
 const addBooksHandler = (request, h) => {
   const {
-    name, year, author, summary, publisher, pageCount, readPage, reading,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
   } = request.payload;
 
   const id = nanoid(16);
@@ -12,12 +19,12 @@ const addBooksHandler = (request, h) => {
   const finished = pageCount === readPage;
 
   // eslint-disable-next-line no-prototype-builtins
-  const cekName = request.payload.hasOwnProperty('name');
+  const cekName = request.payload.hasOwnProperty("name");
 
   if (!cekName) {
     const response = h.response({
-      status: 'fail',
-      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+      status: "fail",
+      message: "Gagal menambahkan buku. Mohon isi nama buku",
     });
     response.code(400);
     return response;
@@ -25,7 +32,18 @@ const addBooksHandler = (request, h) => {
 
   const newBooks = {
     // eslint-disable-next-line max-len
-    name, year, author, summary, publisher, pageCount, readPage, finished, reading, id, insertedAt, updatedAt,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    id,
+    insertedAt,
+    updatedAt,
   };
 
   if (pageCount >= readPage) {
@@ -35,26 +53,28 @@ const addBooksHandler = (request, h) => {
   const isSuccess = books.filter((book) => book.id === id).length > 0;
   if (isSuccess) {
     const response = h.response({
-      status: 'success',
-      message: 'Buku berhasil ditambahkan',
+      status: "success",
+      message: "Buku berhasil ditambahkan",
       data: {
         bookId: id,
       },
     });
     response.code(201);
     return response;
-  } if (readPage > pageCount) {
+  }
+  if (readPage > pageCount) {
     const response = h.response({
-      status: 'fail',
-      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+      status: "fail",
+      message:
+        "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
     });
     response.code(400);
     return response;
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Buku gagal ditambahkan',
+    status: "fail",
+    message: "Buku gagal ditambahkan",
   });
   response.code(500);
   return response;
@@ -71,12 +91,34 @@ const getAllBooksHandler = (request, h) => {
     for (let i = 0; i < books.length; i++) {
       if (books[i].name.toLowerCase().includes(name.toLowerCase())) {
         // eslint-disable-next-line no-shadow
-        const { id, name, publisher } = books[i];
-        array.push({ id, name, publisher });
+        const {
+          id,
+          name,
+          year,
+          author,
+          summary,
+          publisher,
+          pageCount,
+          readPage,
+          finished,
+          reading,
+        } = books[i];
+        array.push({
+          id,
+          name,
+          year,
+          author,
+          summary,
+          publisher,
+          pageCount,
+          readPage,
+          finished,
+          reading,
+        });
       }
     }
     responseBody = {
-      status: 'success',
+      status: "success",
       data: {
         books: array,
       },
@@ -85,7 +127,7 @@ const getAllBooksHandler = (request, h) => {
   }
 
   // eslint-disable-next-line no-mixed-operators
-  if (reading && Number(reading) === 0 || Number(reading) === 1) {
+  if ((reading && Number(reading) === 0) || Number(reading) === 1) {
     const array = [];
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < books.length; i++) {
@@ -93,11 +135,22 @@ const getAllBooksHandler = (request, h) => {
       if (books[i].reading == reading) {
         // eslint-disable-next-line no-shadow
         const { id, name, publisher } = books[i];
-        array.push({ id, name, publisher });
+        array.push({
+          id,
+          name,
+          year,
+          author,
+          summary,
+          publisher,
+          pageCount,
+          readPage,
+          finished,
+          reading,
+        });
       }
     }
     responseBody = {
-      status: 'success',
+      status: "success",
       data: {
         books: array,
       },
@@ -106,7 +159,7 @@ const getAllBooksHandler = (request, h) => {
   }
 
   // eslint-disable-next-line no-mixed-operators
-  if (selesai && Number(selesai) === 0 || Number(selesai) === 1) {
+  if ((selesai && Number(selesai) === 0) || Number(selesai) === 1) {
     const array = [];
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < books.length; i++) {
@@ -118,20 +171,25 @@ const getAllBooksHandler = (request, h) => {
       }
     }
     responseBody = {
-      status: 'success',
+      status: "success",
       data: {
         books: array,
       },
     };
     return responseBody;
-  } if (selesai && Number(selesai) !== 0 && Number(selesai) !== 1) {
+  }
+  if (selesai && Number(selesai) !== 0 && Number(selesai) !== 1) {
     const array = [];
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < books.length; i++) {
-      array.push({ id: books[i].id, name: books[i].name, publisher: books[i].publisher });
+      array.push({
+        id: books[i].id,
+        name: books[i].name,
+        publisher: books[i].publisher,
+      });
     }
     responseBody = {
-      status: 'success',
+      status: "success",
       data: {
         books: array,
       },
@@ -143,10 +201,21 @@ const getAllBooksHandler = (request, h) => {
     const array = [];
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < books.length; i++) {
-      array.push({ id: books[i].id, name: books[i].name, publisher: books[i].publisher });
+      array.push({
+        id: books[i].id,
+        name: books[i].name,
+        publisher: books[i].publisher,
+        year: books[i].year,
+        author: books[i].author,
+        summary: books[i].summary,
+        pageCount: books[i].pageCount,
+        readPage: books[i].readPage,
+        finished: books[i].finished,
+        reading: books[i].reading,
+      });
     }
     responseBody = {
-      status: 'success',
+      status: "success",
       data: {
         books: array,
       },
@@ -154,7 +223,7 @@ const getAllBooksHandler = (request, h) => {
     return responseBody;
   }
   responseBody = {
-    status: 'success',
+    status: "success",
     data: {
       books,
     },
@@ -168,15 +237,15 @@ const getBooksByIdHandler = (request, h) => {
 
   if (book !== undefined) {
     return {
-      status: 'success',
+      status: "success",
       data: {
         book,
       },
     };
   }
   const response = h.response({
-    status: 'fail',
-    message: 'Buku tidak ditemukan',
+    status: "fail",
+    message: "Buku tidak ditemukan",
   });
   response.code(404);
   return response;
@@ -185,29 +254,39 @@ const getBooksByIdHandler = (request, h) => {
 // eslint-disable-next-line consistent-return
 const editBooksByIdHandler = (request, h) => {
   // eslint-disable-next-line no-prototype-builtins
-  const cekNameProperty = request.payload.hasOwnProperty('name');
+  const cekNameProperty = request.payload.hasOwnProperty("name");
   const { readPage, pageCount } = request.payload;
   const cekReadPage = readPage <= pageCount;
 
   if (!cekNameProperty) {
     const response = h.response({
-      status: 'fail',
-      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+      status: "fail",
+      message: "Gagal memperbarui buku. Mohon isi nama buku",
     });
     response.code(400);
     return response;
-  } if (!cekReadPage) {
+  }
+  if (!cekReadPage) {
     const response = h.response({
-      status: 'fail',
-      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+      status: "fail",
+      message:
+        "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
     });
     response.code(400);
     return response;
-  } if (cekNameProperty && cekReadPage) {
+  }
+  if (cekNameProperty && cekReadPage) {
     const { id } = request.params;
     const {
       // eslint-disable-next-line no-shadow
-      name, year, author, summary, publisher, pageCount, readPage, reading,
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
     } = request.payload;
     const updatedAt = new Date().toISOString();
     const index = books.findIndex((book) => book.id === id);
@@ -227,16 +306,16 @@ const editBooksByIdHandler = (request, h) => {
       };
 
       const response = h.response({
-        status: 'success',
-        message: 'Buku berhasil diperbarui',
+        status: "success",
+        message: "Buku berhasil diperbarui",
       });
       response.code(200);
       return response;
     }
 
     const response = h.response({
-      status: 'fail',
-      message: 'Gagal memperbarui buku. Id tidak ditemukan',
+      status: "fail",
+      message: "Gagal memperbarui buku. Id tidak ditemukan",
     });
     response.code(404);
     return response;
@@ -250,16 +329,16 @@ const deleteBooksByIdHandler = (request, h) => {
   if (index !== -1) {
     books.splice(index, 1);
     const response = h.response({
-      status: 'success',
-      message: 'Buku berhasil dihapus',
+      status: "success",
+      message: "Buku berhasil dihapus",
     });
     response.code(200);
     return response;
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Buku gagal dihapus. Id tidak ditemukan',
+    status: "fail",
+    message: "Buku gagal dihapus. Id tidak ditemukan",
   });
   response.code(404);
   return response;
@@ -267,5 +346,9 @@ const deleteBooksByIdHandler = (request, h) => {
 
 module.exports = {
   // eslint-disable-next-line max-len
-  addBooksHandler, getAllBooksHandler, getBooksByIdHandler, editBooksByIdHandler, deleteBooksByIdHandler,
+  addBooksHandler,
+  getAllBooksHandler,
+  getBooksByIdHandler,
+  editBooksByIdHandler,
+  deleteBooksByIdHandler,
 };
--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+require("dotenv").config();
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
